caa03d8d0128eea9a23afb5b8d92f843de4faa7d
iwu589 <iwu589@172.24.155.20>
2019-11-20 09:57:19 -0600

    dryuen's changes

--- module1/scripts/scripts.js
+++ module1/scripts/scripts.js
@@ -118,6 +118,7 @@ function visitTopic7()
 
 function checkAllVisited()
 {
+
 	var t1 = sessionStorage.getItem( "topic1_1" );
 	var t2 = sessionStorage.getItem( "topic1_2" );
 	var t3 = sessionStorage.getItem( "topic1_3" );
@@ -126,6 +127,7 @@ function checkAllVisited()
 	var t6 = sessionStorage.getItem( "topic1_6" );
 	var t7 = sessionStorage.getItem( "topic1_7" );
 	
+	alert( t1 + ", " + t2 + "," + t3 + ", " + t4 + ", " + t5 + "," + t6 + ", " + t7 );
 	if( t1 == "visited" && t2 == "visited" && t3 == "visited" && t4 == "visited" && t5 == "visited" && t6 == "visited" && t7 == "visited" )
 	{
 		document.getElementById( "content-frame" ). contentWindow.documentElementById( "quiz-link" ).style.display = "inline";
