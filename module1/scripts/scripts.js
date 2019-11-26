497ae2bc01609c44ca77ab6f0e4c63e7f91aea63
Ana <Ana@Beautiful.lan>
2019-11-26 17:33:45 -0600

    attempt at the slideshow

--- module1/scripts/scripts.js
+++ module1/scripts/scripts.js
@@ -148,3 +148,24 @@ function finishCourse()
 	oScorm.save();
 	oScorm.quit();
 }
+
+
+var index = 0; 
+
+	$( document ).ready( function() {
+	slideshow();
+	} );
+
+// this function will start the slideshow
+function slideshow()
+{
+	$( ".slideshowimage" ).hide();
+	$( ".slideshowimage" ).eq( index ).show();
+	index = index + 1;
+	if( index == $( ".slideshowimage" ).length )
+{
+	index = 0;
+}
+// the initial time is 3 seconds (3000 milliseconds), but you can change this number
+setTimeout( slideshow, 3000 );		
+}
