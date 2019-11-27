ade0b7be2501763aa53bd589996cce3f9dcef6c4
Ana <Ana@172.31.98.45>
2019-11-27 09:31:35 -0600

    edits

--- module1/scripts/scripts.js
+++ module1/scripts/scripts.js
@@ -162,3 +162,25 @@ function finishCourse()
 	oScorm.save();
 	oScorm.quit();
 }
+
+
+var images= ["images/ilatte1.jpg", "images/ilatte2.jpg", "images/ilatte3.jpg"];
+var texts= ["This is a latte", "THis is a latte 2", "This is a latte 3"];
+var index = 0; 
+	$( document ).ready( function() {
+	slideshow();
+	} );
+
+// this function will start the slideshow
+function slideshow()
+{
+	$( ".slideshowimage" ).hide();
+	$( ".slideshowimage" ).eq( index ).show();
+	index = index + 1;
+	if( index == $( ".slideshowimage" ).length )
+{
+	index = 0;
+}
+// the initial time is 3 seconds (3000 milliseconds), but you can change this number
+setTimeout( slideshow, 3000 );		
+}
