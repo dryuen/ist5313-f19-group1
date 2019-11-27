cd99b31106ed25f039da5c1c46fde3c4e21e54fa
iwu589 <iwu589@172.24.154.155>
2019-11-27 10:28:35 -0600

    commit

--- module1/scripts/quiz.js
+++ module1/scripts/quiz.js
@@ -1,1 +1,43 @@
-test
+function processQuiz()
+{
+	var answer1 = document.forms.quiz1.question1.value;
+	var answer2 = document.forms.quiz1.question2.value;
+	var answer3 = document.forms.quiz1.question3.value;
+	var answer4 = document.forms.quiz1.question4.value;
+	var answer5 = document.forms.quiz1.question5.value;
+	var counter = 0;
+	if( answer1 == 2 )
+	{
+	counter = counter + 1;
+	}
+	if( answer2 == 0 )
+	{
+	counter = counter + 1;
+	}
+	if( answer3 == 1 )
+	{
+	counter = counter + 1;
+	}
+	if( answer4 == 2 )
+	{
+	counter = counter + 1;
+	}
+	if( answer5 == 2 )
+	{
+	counter = counter + 1;
+	}
+	var percentage = counter * 20;
+	
+	if( percentage >= 80 )
+	{
+		alert( "Your score is " + percentage + "%. You Passed." );
+		document.getElementById( "content-frame" ).contentWindow.document.getElementById( "certificate-link" ).style.display = "inline";
+	}
+	else
+	{
+		alert( "Your score is " + percentage + "%. You Failed." );
+	}
+	//here is some extra content
+	
+	//parent.reportScores( percentage );
+}
